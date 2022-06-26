545bbe207aae6936dbd791d36b6dcddd7582eace
Tyrae Yao <yaotyrae@gmail.com>
2022-06-26 15:59:52 -0700

    Webhook test

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -10,9 +10,13 @@ const SIG_KEY = process.env.NEXT_PUBLIC_SIGNATURE_KEY;
 // Function to generate signature from url and body and compare to square signature.
 function isFromSquare(sigKey, notificationUrl, squareSignature, rawBody) {
   // create hmac signature
+  console.log("hmac");
   const hmac = crypto.createHmac("sha1", sigKey);
+  console.log("after hmac");
   hmac.update(notificationUrl + rawBody);
+  console.log("hmac update");
   const hash = hmac.digest("base64");
+  console.log("hash return");
 
   // compare to square signature
   return hash === squareSignature;
@@ -20,24 +24,28 @@ function isFromSquare(sigKey, notificationUrl, squareSignature, rawBody) {
 
 export default function handler(req, res) {
   if (req.method === "POST") {
+    console.log("IF");
     let rawBody = "";
     req.setEncoding("utf8");
-
+    console.log("setencoding");
     req.on("data", function (chunk) {
       rawBody += chunk;
     });
     const squareSignature = req.headers["x-square-signature"];
+    console.log("square sig");
     const eventIsFromSquare = isFromSquare(
       SIG_KEY,
       NOTIFICATION_URL,
       squareSignature,
       rawBody
     );
-
+    console.log("eventfromsquare");
     if (eventIsFromSquare) {
+      console.log("if event from square");
       res.writeHead(200);
       res.write("Signature is valid. \n");
     } else {
+      console.log("else event from square");
       res.writeHead(400);
       res.write("Signature is not valid \n");
     }
