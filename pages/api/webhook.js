12cd01aa91a557c5e37abf6ef9f70f677a0ed999
Tyrae Yao <yaotyrae@gmail.com>
2022-06-28 17:10:24 -0700

    WH

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -40,7 +40,6 @@ export default async function handler(req, res) {
       res.writeHead(200);
       res.write("Signature is valid. \n");
       console.log(res);
-      console.log(req);
     } else {
       res.writeHead(400);
       res.write("Signature is not valid \n");
