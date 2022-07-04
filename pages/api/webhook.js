2af1e35789ab14946336e39332410882fc099643
Tyrae Yao <yaotyrae@gmail.com>
2022-07-04 13:38:27 -0700

    Micro

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,8 +1,11 @@
+import { text } from "body-parser";
+
 // The crypto module provides cryptographic functionality.
 const crypto = require("crypto");
 const http = require("http");
 const getRawBody = require("raw-body");
 const bodyParser = require("body-parser");
+const { buffer, txt } = requre("micro");
 
 // The URL where event notifications are sent.
 const NOTIFICATION_URL = "https://next-square-dashboard.vercel.app/api/webhook";
@@ -23,23 +26,28 @@ function isFromSquare(sigKey, notificationUrl, squareSignature, rawBody) {
 export default async function handler(req, res) {
   if (req.method === "POST") {
     // Use raw-body to get rawBody because nextjs dont work with it :(
-    const rawBody = await getRawBody(req);
-    req.setEncoding("utf8");
+    // const rawBody = await getRawBody(req);
+    // req.setEncoding("utf8");
+
+    const buf = await buffer(req);
+
     req.on("data", function (chunk) {
-      rawBody += chunk;
+      //rawBody += chunk;
+      buf += chunk;
     });
     const squareSignature = req.headers["x-square-signature"];
     const eventIsFromSquare = isFromSquare(
       SIG_KEY,
       NOTIFICATION_URL,
       squareSignature,
-      rawBody
+      //rawBody
+      buf
     );
     if (eventIsFromSquare) {
       res.writeHead(200);
       res.write("Signature is valid. \n");
-      console.log(bodyParser.json());
-      console.log(JSON.stringify(req.body));
+      const txt = await text(req);
+      console.log(txt);
     } else {
       res.writeHead(400);
       res.write("Signature is not valid \n");
