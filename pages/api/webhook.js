8ee217f6057cf5feb20a8a4e39931f24d03b2221
Tyrae Yao <yaotyrae@gmail.com>
2022-07-05 20:09:36 -0700

    NodeJS WH

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,8 +2,29 @@
 const crypto = require("crypto");
 const http = require("http");
 const { buffer, text, json } = require("micro");
-import { storePayments, db } from "../../firebase/clientApp";
-require("firebase/firestore");
+// import { storePayments, db } from "../../firebase/clientApp";
+
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+
+initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+});
+
+const db = getFirestore();
 
 // The URL where event notifications are sent.
 const NOTIFICATION_URL = "https://next-square-dashboard.vercel.app/api/webhook";
@@ -53,14 +74,14 @@ export default async function handler(req, res) {
         payment_id: js.data.object.payment.id,
         data_payment_amount: js.data.object.payment.amount_money.amount,
       });
-      storePayments(
-        js.merchant_id,
-        js.created_at,
-        js.event_id,
-        js.data.id,
-        js.data.object.payment.id,
-        js.data.object.payment.amount_money.amount
-      );
+      // storePayments(
+      //   js.merchant_id,
+      //   js.created_at,
+      //   js.event_id,
+      //   js.data.id,
+      //   js.data.object.payment.id,
+      //   js.data.object.payment.amount_money.amount
+      // );
     } else {
       res.writeHead(400);
       res.write("Signature is not valid \n");
