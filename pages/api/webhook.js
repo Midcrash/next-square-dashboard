c7d16b8110d8e648a722d672d0062795c48eb9b8
Tyrae Yao <yaotyrae@gmail.com>
2022-07-05 20:55:43 -0700

    Updated Webhook service account

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -5,8 +5,24 @@ const { buffer, text, json } = require("micro");
 // import { storePayments, db } from "../../firebase/clientApp";
 
 const { initializeApp, cert } = require("firebase-admin/app");
-
-var serviceAccount = require("../../data/next-dashboard-e43a9-firebase-adminsdk-uer1o-0746c35399.json");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+var serviceAccount = {
+  type: process.env.NEXT_PUBLIC_TYPE,
+  project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
+};
 
 initializeApp({
   credential: cert(serviceAccount),
