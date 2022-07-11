959ec5c95dd2c6d922e2770bb167dc6687971e7c
Tyrae Yao <yaotyrae@gmail.com>
2022-07-10 23:41:53 -0700

    retrieve image

--- pages/api/serverApp.js
+++ pages/api/serverApp.js
@@ -39,8 +39,16 @@ const storeOrders = async (js) => {
   }
 };
 
-const storeMenu = async (itemData) => {
-  console.dir(itemData);
+const storeMenu = async (itemData, client) => {
+  try {
+    const response = await client.catalogApi.retrieveCatalogObject(
+      itemData.image_ids
+    );
+
+    console.log(response.result);
+  } catch (error) {
+    console.log(error);
+  }
   // const docRef = db.collection("SquareMenu").doc();
   // await docRef.set({
   //   name: itemData.name,
